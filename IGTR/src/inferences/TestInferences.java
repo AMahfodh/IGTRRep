92bd13d5097248e9c35d9882839980d2fb5db203
kehrer <kehrer@informatik.uni-siegen.de>
2016-10-19 09:15:27 +0200

    Intermediate state

--- IGTR/src/inferences/TestInferences.java
+++ IGTR/src/inferences/TestInferences.java
@@ -1,6 +1,5 @@
 package inferences;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -8,61 +7,55 @@ import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+import emf.domain.DomainConfigurationFactory;
+
 public class TestInferences {
 
-	
-	public void commitLearning() throws Exception{
-				
-		// commit generalisation 		
-		int iMinRepeatedMultiObjects =1;
+	public void commitLearning() throws Exception {
+
+		// commit generalisation
+		int iMinRepeatedMultiObjects = 1;
 		new RuleInference().generaliseRuleInstance(iMinRepeatedMultiObjects);
-		
-		
-		
-		// learning invariant constraints on rule attributes and parameters 
-		new AttributeInference().commit();		
-		
-				
-		
-		
+
+		// learning invariant constraints on rule attributes and parameters
+		if (!DomainConfigurationFactory.createDomainConfiguration().treatAttributesAsNodes()) {
+			new AttributeInference().commit();
+		}
+
 		// export all rules to Henshin
-		//ExportAllToHenshin.main(
-		//		new String[]{
-		//				new File("").getAbsolutePath() + File.separator + "HenshinOutput"});
+		// ExportAllToHenshin.main(
+		// new String[]{
+		// new File("").getAbsolutePath() + File.separator + "HenshinOutput"});
 
 	}
-	
-	public static void main(String[] args) throws Exception {
-		
-		
-		Logger GTlogger = Logger.getLogger("GTlog"); 
-	    try {  
-	         
-	    	boolean isAppend=false;
-	    	FileHandler fh = new FileHandler("GT.log", isAppend);
-	        	    	
-	    	GTlogger.addHandler(fh);
-	        SimpleFormatter formatter = new SimpleFormatter() {
-	        	public String format(LogRecord record) {
-	        		return record.getMessage() + System.lineSeparator();	        		
-	             }
-	        };
-	        GTlogger.setLevel(Level.ALL);
-	        fh.setFormatter(formatter); 
-	        //GTlogger.addHandler(new ConsoleHandler());
-	        GTlogger.setUseParentHandlers(false);	       
 
-	    } 
-	    catch (SecurityException e) { e.printStackTrace(); }
-	    catch (IOException e) {  e.printStackTrace(); }
-		
-		
-		
-		
+	public static void main(String[] args) throws Exception {
+
+		Logger GTlogger = Logger.getLogger("GTlog");
+		try {
+
+			boolean isAppend = false;
+			FileHandler fh = new FileHandler("GT.log", isAppend);
+
+			GTlogger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter() {
+				public String format(LogRecord record) {
+					return record.getMessage() + System.lineSeparator();
+				}
+			};
+			GTlogger.setLevel(Level.ALL);
+			fh.setFormatter(formatter);
+			// GTlogger.addHandler(new ConsoleHandler());
+			GTlogger.setUseParentHandlers(false);
+
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		new TestInferences().commitLearning();
 
 	}
-
-
 
 }
