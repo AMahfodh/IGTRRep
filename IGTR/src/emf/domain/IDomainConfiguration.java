c098cebdd5fabdcb44a03cb6eef7a8e93c8b31bb
Timo Kehrer <timo.kehrer@informatik.hu-berlin.de>
2017-11-09 15:35:24 +0100

    model type uml -> ecore

--- IGTR/src/emf/domain/IDomainConfiguration.java
+++ IGTR/src/emf/domain/IDomainConfiguration.java
@@ -17,8 +17,8 @@ import emf.matching.IMatcher;
  */
 public interface IDomainConfiguration {
 
-	//public static final String MODEL_TYPE = "ecore";
-	public static final String MODEL_TYPE = "uml";
+	public static final String MODEL_TYPE = "ecore";
+	//public static final String MODEL_TYPE = "uml";
 	
 	public Set<EClass> getUnconsideredNodeTypes();
 
